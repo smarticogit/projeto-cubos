47a54229c0e53a72c7c8655060e055bc8162b7f0
Daniel <daniel.repres@hotmail.com>
2021-10-19 15:57:46 -0300

    config Conexao

--- conexao.js
+++ conexao.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '123456',
-        database: 'market_cubos'
+        host: 'ec2-3-222-24-200.compute-1.amazonaws.com',
+        user: 'kvzlxspdmfvsbn',
+        port: 5432,
+        password: '48226745a486d791801115e0d3618155ac5d852f700c77fe72d45f9a9a34cd16',
+        database: 'damhk06pjjflqi',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
